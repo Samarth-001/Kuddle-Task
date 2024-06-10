9e14decae6e0e99c5b55df0a24d1dafd9ed21494
Samarth Agarwal <88262012+Samarth-001@users.noreply.github.com>
2024-06-10 18:59:10 +0530

    Add files via upload

--- Task.cpp
+++ Task.cpp
@@ -1,52 +1,65 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <limits.h>
+
 using namespace std;
 
 int main() {
-    // Write C++ code here
     int test;
-    cin>>test;
-    int n,k;
-    for(int t=0;t<test;t++){
+    cin >> test;
+    
+    while (test--) {
+        int n, k;
+        cin >> n >> k;
         
-        cin>>n;
-        cin>>k;
-        int a[n] = {0};
-        int b[n] = {0};
-        int maximum = 0;
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-            maximum = max(maximum,a[i]);
-        }
-        for(int i=0;i<n;i++){
-            cin>>b[i];
-        }
-        int c[maximum+1] = {0};
+        vector<int> categories(n);
+        vector<int> cooking_times(n);
         
-        for(int i=0 ; i<n; i++ ){
-            if(a[a[i]]<b[i]){
-            c[i] = b[i];
+        for (int i = 0; i < n; ++i) {
+            cin >> categories[i];
+        }
+        
+        for (int i = 0; i < n; ++i) {
+            cin >> cooking_times[i];
+        }
+        
+        // Use a map to store the minimum cooking time for each category
+        unordered_map<int, int> min_time_per_category;
+        
+        for (int i = 0; i < n; ++i) {
+            int category = categories[i];
+            int time = cooking_times[i];
+            
+            if (min_time_per_category.find(category) == min_time_per_category.end() ||
+                time < min_time_per_category[category]) {
+                min_time_per_category[category] = time;
             }
         }
         
-        for(int i=0; i<n; i++){
-            int ind = i;
-            for(int j=0;j<n;j++){
-                if(c[j]<c[ind]){
-                    ind = j;
-                }
-            }
-            int temp = c[i];
-            c[i] = c[ind];
-            c[ind] = temp;
+        // If the number of distinct categories is less than k, it's not possible to have a complete meal
+        if (min_time_per_category.size() < k) {
+            cout << -1 << endl;
+            continue;
         }
         
-        int sum = 0;
-        for(int i = 0; i < k; i++){
-            sum += c[i];
+        // Collect all minimum times in a vector and sort it
+        vector<int> min_times;
+        for (auto it = min_time_per_category.begin(); it != min_time_per_category.end(); ++it) {
+            min_times.push_back(it->second);
         }
-        cout << sum;
-    }    
         
+        sort(min_times.begin(), min_times.end());
+        
+        // Sum up the smallest k times
+        int total_time = 0;
+        for (int i = 0; i < k; ++i) {
+            total_time += min_times[i];
+        }
+        
+        cout << total_time << endl;
+    }
+    
     return 0;
 }
